5bd09aedbe5b03095a36e4e45223150b84675b62
Barisere Jonathan <barry.jonathanks@gmail.com>
2020-06-01 03:01:04 +0100

    Demonstrate the context approach.
    An interesting effect of the context approach is that
    we cannot test its full execution path by using a ResponseRecorder.
    We must create a server and send requests to it
    in order to test the entire middleware configuration.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,93 +13,96 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-type User struct {
+type userAccount struct {
 	gorm.Model
 	Nickname string `gorm:"type:varchar(20);unique;not null"`
 }
 
-type UserRepository interface {
-	AddUser(User) error
-	GetByNickname(string) (*User, error)
-	ChangeNickname(User, string) error
+type userRepository interface {
+	AddUser(userAccount) error
+	GetByNickname(string) (*userAccount, error)
+	ChangeNickname(userAccount, string) error
 }
 
-type UserDB struct {
+type userDB struct {
 	db *gorm.DB
 }
 
-func (u UserDB) AddUser(user User) error {
+func (u userDB) AddUser(user userAccount) error {
 	return u.db.Create(&user).Error
 }
 
-func (u UserDB) GetByNickname(nick string) (*User, error) {
-	var user User
-	if u.db.First(&user, User{Nickname: nick}).RecordNotFound() {
+func (u userDB) GetByNickname(nick string) (*userAccount, error) {
+	var user userAccount
+	if u.db.First(&user, userAccount{Nickname: nick}).RecordNotFound() {
 		return nil, fmt.Errorf("no user found")
 	}
 	return &user, nil
 }
 
-func (u UserDB) ChangeNickname(user User, newNick string) error {
+func (u userDB) ChangeNickname(user userAccount, newNick string) error {
 	return u.db.Model(&user).Update("Nickname", newNick).Error
 }
 
-func ConnectToDB(dbPath string) (*gorm.DB, error) {
+func connectToDB(dbPath string) (*gorm.DB, error) {
 	db, err := gorm.Open("sqlite3", dbPath)
 	if err != nil {
 		return nil, fmt.Errorf("opening database failed with error: %w", err)
 	}
-	if err = db.AutoMigrate(&User{}).Error; err != nil {
+	if err = db.AutoMigrate(&userAccount{}).Error; err != nil {
 		return nil, fmt.Errorf("migrating models failed with error: %w", err)
 	}
 	return db, nil
 }
 
 // Authentication shall establish a user's identity for an HTTP request.
-// Assume it supports only HTTP Bearer authentication, but it can be abstracted
-// into an interface for which several authentication mechanisms can be implemented.
+// It supports only HTTP Bearer authentication, but it can be extended to
+// support several authentication mechanisms can be implemented.
 type Authentication struct {
-	currentUser *User
-	userRepo    UserRepository
+	userRepo userRepository
 }
 
-func (authn Authentication) userFromRequest(r *http.Request) (*User, error) {
+func (authn Authentication) userFromRequest(r *http.Request) (*userAccount, error) {
 	nick, _, ok := r.BasicAuth()
 	if !ok {
 		return nil, fmt.Errorf("no user credentials provided")
 	}
-	var err error
-	authn.currentUser, err = authn.userRepo.GetByNickname(nick)
-
-	return authn.currentUser, err
+	return authn.userRepo.GetByNickname(nick)
 }
 
-// ServeHTTP allows us to use Authentication as an http.Handler and to compose it
-// with other request handlers as a middleware. We don't do that here, but we write
-// this method to demonstrate that it's possible to fall back to http.Handler composition.
-//func (authn Authentication) middleware(next http.Handler) http.Handler {
-//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		var err error
-//		if authn.currentUser, err = authn.userFromRequest(r); err != nil {
-//			// end request if authentication credentials are invalid
-//		}
-//	})
-//}
+type userCtxKey string
 
-type UserAccountController struct {
-	userRepo UserRepository
+func (authn Authentication) setUserMidddleware(next http.Handler) http.Handler {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		user, err := authn.userFromRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		ctx := context.WithValue(r.Context(), userCtxKey("user"), user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+	return http.HandlerFunc(handler)
 }
 
-func NewUserAccountcontroller(db *gorm.DB) UserAccountController {
-	return UserAccountController{userRepo: UserDB{db: db}}
+type userAccountController struct {
+	userRepo userRepository
 }
 
-func (rg UserAccountController) changeNickname(w http.ResponseWriter, r *http.Request) {
+func newUserAccountcontroller(db *gorm.DB) userAccountController {
+	return userAccountController{userRepo: userDB{db: db}}
+}
+
+func (rg userAccountController) changeNickname(w http.ResponseWriter, r *http.Request) {
 	user, err := Authentication{userRepo: rg.userRepo}.userFromRequest(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
+
+	pathSegments := strings.Split(r.URL.Path, "/")
+	urlNick := strings.TrimSpace(pathSegments[len(pathSegments)-1])
+
 	// Authentication is done at this point; authorisation can be performed here if necessary.
 	// This approach makes it obvious in the request handler that authentication is needed.
 	// Authorisation is often not as generic as authentication, so it makes sense to do it
@@ -106,67 +110,93 @@ func (rg UserAccountController) changeNickname(w http.ResponseWriter, r *http.Re
 	// we know that a delete operation has been attempted.
 	// If the authorisation rules can be generalised for operations on this resource, they can
 	// be factored out into methods on the resource.
-	pathSegments := strings.Split(r.URL.Path, "/")
-	urlNick := strings.TrimSpace(pathSegments[len(pathSegments)-1])
 	if urlNick != user.Nickname {
 		http.Error(w, "You cannot change the Nickname for this account!", http.StatusUnauthorized)
 		return
 	}
 
-	if err = r.ParseForm(); err != nil {
-		http.Error(w, "Could not process your request", http.StatusBadRequest)
-		return
-	}
-	newNick := r.PostFormValue("nickname")
+	newNick := r.FormValue("nickname")
 	if newNick == "" {
 		http.Error(w, "Nickname cannot be empty", http.StatusBadRequest)
 		return
 	}
+
 	if err = rg.userRepo.ChangeNickname(*user, newNick); err != nil {
 		log.Printf("error changing user Nickname: %v", err)
 		http.Error(w, "Oops! Try again later.", http.StatusInternalServerError)
 		return
 	}
+
 	w.Write([]byte("Nickname successfully changed"))
 	return
+}
 
-	// Compare with the alternative
+func (rg userAccountController) changeNicknameCtx(w http.ResponseWriter, r *http.Request) {
+	// This approach uses the request context access the request user.
 	//
 	// First, the context value is set in a separate middleware. If by misconfiguration it happened
 	// to be absent, we have to trace the problem to that handler configuration rather than look a
 	// few lines up.
-	// ctx := r.Context()
-	// user := ctx.Value("user")
-	// if user == nil {
-	// 	// end request
-	// }
 	// Also, Context values are not type safe, so we cannot be certain that what we got is of the
 	// correct type; do you trust programmer consistency that much? A type assertion is necessary.
 	// This type assertion would be done in every handler where the user info is passed in Context.
-	// _user, ok := user.(User)
-	// if !ok {
-	// 	// handle cast error
-	// }
+	user, ok := r.Context().Value(userCtxKey("user")).(*userAccount)
+	if !ok {
+		log.Println("'user' was not set in context")
+		http.Error(w, "Oops. Please try again later.", http.StatusInternalServerError)
+		return
+	}
+
+	pathSegments := strings.Split(r.URL.Path, "/")
+	urlNick := strings.TrimSpace(pathSegments[len(pathSegments)-1])
+
+	if urlNick != user.Nickname {
+		http.Error(w, "You cannot change the Nickname for this account!", http.StatusUnauthorized)
+		return
+	}
+
+	newNick := r.FormValue("nickname")
+	if newNick == "" {
+		http.Error(w, "Nickname cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	if err := rg.userRepo.ChangeNickname(*user, newNick); err != nil {
+		log.Printf("error changing user Nickname: %v", err)
+		http.Error(w, "Oops! Try again later.", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Nickname successfully changed"))
+	return
+}
+
+func setupRouter(db *gorm.DB) *mux.Router {
+	accounts := newUserAccountcontroller(db)
+
+	r := mux.NewRouter()
+
+	authn := Authentication{userRepo: userDB{db: db}}
+
+	// For PUT requests we pass the request user in the request context.
+	r.Handle(
+		"/user/{nick}",
+		authn.setUserMidddleware(http.HandlerFunc(accounts.changeNicknameCtx)),
+	).Methods(http.MethodPut)
+
+	// For PATCH requests we fetch the request user in the handler.
+	r.HandleFunc("/user/{nick}", accounts.changeNickname).Methods(http.MethodPatch)
+
+	return r
 }
 
 func main() {
-	db, err := ConnectToDB("demo_db.sqlite3")
+	db, err := connectToDB("demo_db.sqlite3")
 	if err != nil {
 		panic(err)
 	}
 
-	// Any operation defined on UserAccountController will share the database connection.
-	accounts := NewUserAccountcontroller(db)
-
-	r := mux.NewRouter()
-
-	// For middleware that do not pass values further down, we can use http.Handler composition.
-	// We can even register Authentication on the Router as follows.
-	// Of course this is not useful: we rarely want to check authentication without using the
-	// user identification we get in the operation.
-	//r.Use(Authentication{userRepo: &UserDB{db: db}}.middleware)
-	// We pass the RestrictedGallery delete method here, because we use the value in the request context.
-	r.HandleFunc("/user/{nick}", accounts.changeNickname).Methods(http.MethodPatch)
+	r := setupRouter(db)
 
 	server := http.Server{Handler: r, Addr: "localhost:8080", ReadTimeout: time.Second * 10}
 	if err := server.ListenAndServe(); err != nil {
--- main_test.go
+++ main_test.go
@@ -1,58 +1,116 @@
 package main
 
 import (
-	"github.com/jinzhu/gorm"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
+
+	"github.com/jinzhu/gorm"
 )
 
 func sqliteInMemory(t *testing.T) *gorm.DB {
 	t.Helper()
-	db, err := ConnectToDB(":memory:")
+	db, err := connectToDB(":memory:")
 	if err != nil {
 		t.Fatalf("expected to open database successfully, got error %v", err)
 	}
-	t.Cleanup(func () {
+	t.Cleanup(func() {
 		if err = db.Close(); err != nil {
 			t.Logf("closing database returned error: %v", err)
 		}
 	})
 
-	UserDB{db:db}.AddUser(User{Nickname: "joe"})
+	userDB{db: db}.AddUser(userAccount{Nickname: "joe"})
 
 	return db
 }
 
 func runChangeNicknameTest(path string, basicAuthUsername string, newNick string, expectedCode int, t *testing.T) {
-	req := httptest.NewRequest(http.MethodPatch, path, nil)
-	req.PostForm = url.Values{ "nickname": []string{newNick}}
+	formData := url.Values{"nickname": {newNick}}.Encode()
+	req := httptest.NewRequest(http.MethodPatch, path, strings.NewReader(formData))
+	req.Header["Content-Type"] = []string{"application/x-www-form-urlencoded"}
 	req.SetBasicAuth(basicAuthUsername, "")
-	rec := httptest.NewRecorder()
+
 	db := sqliteInMemory(t)
-	accounts := NewUserAccountcontroller(db)
+	accounts := newUserAccountcontroller(db)
+	res := httptest.NewRecorder()
 
-	accounts.changeNickname(rec, req)
+	accounts.changeNickname(res, req)
 
-	if rec.Code != expectedCode {
-		t.Errorf("expected status code %d, got %d", expectedCode, rec.Code)
+	if res.Code != expectedCode {
+		t.Errorf("expected status code %d, got %d", expectedCode, res.Code)
 	}
 }
 
-func Test_ChangeNickname_With_Incorrect_Auth_Fails(t *testing.T) {
-	runChangeNicknameTest("/user/joe", "not_existing", "new_nick", http.StatusUnauthorized, t)
+func Test_ChangeNickname_With_Explicit_Auth_Access(t *testing.T) {
+	t.Run("Given incorrect credentials, it returns status 401", func(t *testing.T) {
+		runChangeNicknameTest("/user/joe", "not_existing", "new_nick", http.StatusUnauthorized, t)
+	})
+
+	t.Run("Given correct credentials, it returns status 200", func(t *testing.T) {
+		runChangeNicknameTest("/user/joe", "joe", "new_nick", http.StatusOK, t)
+	})
+
+	t.Run("Forbids a user from changing other user's nicknames", func(t *testing.T) {
+		runChangeNicknameTest("/user/not_joe", "joe", "new_nick", http.StatusUnauthorized, t)
+	})
+
+	t.Run("Does not allow empty strings for nicknames", func(t *testing.T) {
+		runChangeNicknameTest("/user/joe", "joe", "", http.StatusBadRequest, t)
+	})
 }
 
-func Test_ChangeNickname_With_Correct_Auth_Succeeds(t *testing.T) {
-	runChangeNicknameTest("/user/joe", "joe", "new_nick", http.StatusOK, t)
+func runChangeNicknameCtxTest(path string, basicAuthUsername string, newNick string, expectedCode int, t *testing.T) {
+	db := sqliteInMemory(t)
+	r := setupRouter(db)
+
+	// An interesting effect of the context approach is that
+	// we cannot test its full execution path by using a ResponseRecorder.
+	// We must create a server and send requests to it
+	// in order to test the entire middleware configuration.
+	server := httptest.NewServer(r)
+	defer server.Close()
+
+	formData := url.Values{"nickname": {newNick}}.Encode()
+	req, err := http.NewRequest(http.MethodPut, server.URL+path, strings.NewReader(formData))
+	if err != nil {
+		t.Fatalf("constructing request failed with error: %v", err)
+	}
+	req.Header["Content-Type"] = []string{"application/x-www-form-urlencoded"}
+	req.SetBasicAuth(basicAuthUsername, "")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("could not send request; error: %v", err)
+	}
+	_, err = ioutil.ReadAll(res.Body)
+	err = res.Body.Close()
+	if err != nil {
+		t.Fatalf("could not read or close response body; error: %v", err)
+	}
+
+	if res.StatusCode != expectedCode {
+		t.Errorf("expected status code %d, got %d", expectedCode, res.StatusCode)
+	}
 }
 
-func Test_User_Cannot_Change_Other_User_Nickname(t *testing.T) {
-	runChangeNicknameTest("/user/not_joe", "joe", "new_nick", http.StatusUnauthorized, t)
-}
+func Test_ChangeNickname_With_Implicit_Auth_Access_Via_Context(t *testing.T) {
+	t.Run("Given incorrect credentials, it returns status 401", func(t *testing.T) {
+		runChangeNicknameCtxTest("/user/joe", "not_existing", "new_nick", http.StatusUnauthorized, t)
+	})
 
-func Test_Nickname_Cannot_Be_Empty(t *testing.T) {
-	runChangeNicknameTest("/user/joe", "joe", "", http.StatusBadRequest, t)
-}
+	t.Run("Given correct credentials, it returns status 200", func(t *testing.T) {
+		runChangeNicknameCtxTest("/user/joe", "joe", "new_nick", http.StatusOK, t)
+	})
 
+	t.Run("Forbids a user from changing other user's nicknames", func(t *testing.T) {
+		runChangeNicknameCtxTest("/user/not_joe", "joe", "new_nick", http.StatusUnauthorized, t)
+	})
+
+	t.Run("Does not allow empty strings for nicknames", func(t *testing.T) {
+		runChangeNicknameCtxTest("/user/joe", "joe", "", http.StatusBadRequest, t)
+	})
+}
